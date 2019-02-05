6abdd63c3c23ff6e99c9c07f88a151e8d2edf87a
suusan11 <gawow11@gmail.com>
2019-02-05 00:08:20 -0800

    create component app

---
+++ components/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail />
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
