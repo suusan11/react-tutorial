81e77095dc93ac165a67ce14496448ccad808c90
suusan11 <gawow11@gmail.com>
2019-02-08 08:41:43 -0800

    add statement text and icon

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -10,12 +10,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
 
     return (
         <div>
-            <h1>{ season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach' }</h1>
+            <i className = {`${icon} icon`} />
+            <h1>{ text }</h1>
+            <i className = {`${icon} icon`} />
         </div>
     );
 };
 
-export default SeasonDisplay
+export default SeasonDisplay;
