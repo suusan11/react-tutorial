c091cc415edbf3986f63be573712ea8b1ee558ad
suusan11 <gawow11@gmail.com>
2019-02-15 09:08:36 -0800

    fix callback

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,7 +4,10 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+
+    state = { images: [] };
+
+    onSearchSubmit = async term => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {
                 query: term
@@ -14,12 +17,14 @@ class App extends React.Component {
             }
         });
 
-        console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
+
     render() {
         return(
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
