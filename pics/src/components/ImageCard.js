669209c3ee87e232cadd7d4d4d7492c1b125ec08
suusan11 <gawow11@gmail.com>
2019-03-28 20:15:59 -0700

    add ref

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef);
+    }
 
     render() {
         const {description, urls} = this.props.image;
@@ -8,6 +17,7 @@ class ImageCard extends React.Component {
         return(
             <div>
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
