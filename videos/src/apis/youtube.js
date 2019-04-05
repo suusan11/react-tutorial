6ea7b6841ac2ba439a68965516c665a0476410cd
suusan11 <gawow11@gmail.com>
2019-04-04 17:53:31 -0700

    install axios

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCvoGK-Uoivp0pJ8OPIQ5zwr00i_ViM4Qw';
+
+export default axios.create({
+   baseURL = 'https://www.googleapis.com/youtube/v3',
+    params: {
+       part: 'snippet',
+        maxResult: 5,
+        key: KEY,
+        q: 'surfboards'
+    }
+});
