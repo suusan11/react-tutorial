9ed2e7d8b525d1ce7cd0964a34836883ccca15a3
suusan11 <gawow11@gmail.com>
2019-04-07 00:04:50 -0700

    fix error

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = 'AIzaSyCvoGK-Uoivp0pJ8OPIQ5zwr00i_ViM4Qw';
 
 export default axios.create({
-   baseURL = 'https://www.googleapis.com/youtube/v3',
+   baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
        part: 'snippet',
         maxResult: 5,
